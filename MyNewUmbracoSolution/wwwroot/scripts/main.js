77512bf92be443daddf16b32d10f8b50deaee4a6
charlie1151 <yutongleehs@gmail.com>
2024-08-22 11:49:01 +0800

    上一個commit沒commit好

---
+++ MyNewUmbracoSolution/wwwroot/scripts/main.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('click', function (e) {
+    // Hamburger menu
+    if (e.target.classList.contains('hamburger-toggle')) {
+        e.target.children[0].classList.toggle('active');
+    }
+});
